2924cd65fc9ecb1007b50fd770010b9582758d99
Artii15 <artur.kowalski15@gmail.com>
2018-03-15 05:20:36 +0100

    SnS notifier

---
+++ aws/sns/sns.go
@@ -0,0 +1,26 @@
+package sns
+
+import (
+	"github.com/aws/aws-sdk-go/service/sns"
+	"github.com/aws/aws-sdk-go/aws/client"
+	"github.com/aws/aws-sdk-go/aws"
+)
+
+type Notifier struct {
+	topic string
+	snsService *sns.SNS
+}
+
+func New(awsSession client.ConfigProvider, topic string) *Notifier {
+	snsService := sns.New(awsSession)
+	return &Notifier{topic:topic, snsService:snsService}
+}
+
+func (notifier *Notifier) notify(message string) {
+	snsServiceParams := &sns.PublishInput{
+		Message: aws.String(message),
+		TopicArn: aws.String("arn:aws:sns:us-west-2:632826021673:akowalski-watchdog-topic"),
+	}
+
+	notifier.snsService.Publish(snsServiceParams)
+}
