5f65166e9b5ec5105fbfce67317743052bb165d7
Artii15 <artur.kowalski15@gmail.com>
2018-03-15 22:08:07 +0100

    Graceful exit

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"time"
 	"watchdog/checker"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 var programSettings *config.ProgramSettings
@@ -15,10 +18,11 @@ var loggersObject *loggers.Loggers
 var dynamoLoader *dynamo.ConfigLoader
 var snsNotifier *sns.Notifier
 
+const configRefreshingIntervalInMinutes = 15
+
 func init() {
 	programSettings = config.LoadProgramSettings()
 	loggersObject = loggers.New(programSettings.LogfileLocation)
-	//TODO verify settings and log errors
 
 	awsSession, sessionError := session.NewSession()
 	if sessionError != nil {
@@ -30,22 +34,46 @@ func init() {
 	}
 }
 
+func reloadConfig(configChannel chan<- *checker.Config)  {
+	newConfig, dynamoError := dynamoLoader.ReloadConfig()
+	if dynamoError == nil {
+		loggersObject.Error.Println("Could not load new configuration from dynamo db", dynamoError)
+	} else {
+		loggersObject.Info.Println("New configuration fetched from dynamoDb")
+		configChannel<- newConfig
+	}
+}
+
 func main()  {
 	loggersObject.Info.Println("Fetching config from dynamoDb")
-	//checkerConfig, dynamoErr := dynamoLoader.ReloadConfig()
-	//if dynamoErr != nil {
-	//	loggersObject.Error.Println("Could not fetch config from dynamoDb", dynamoErr)
-	//	panic("Retrieving information about configuration was not possible")
-	//}
-	checkerConfig := checker.Config{NumOfSecCheck:1, ListOfServices:[]string{"docker"}, NumOfAttempts:2, NumOfSecWait:1}
+	checkerConfig, dynamoErr := dynamoLoader.ReloadConfig()
+	if dynamoErr != nil {
+		loggersObject.Error.Println("Could not fetch config from dynamoDb", dynamoErr)
+		panic("Retrieving information about configuration was not possible")
+	}
+
 	servicesCheckingTicker := time.NewTicker(time.Duration(checkerConfig.NumOfSecCheck) * time.Second)
 	defer servicesCheckingTicker.Stop()
 
+	configCheckingTicker := time.NewTicker(configRefreshingIntervalInMinutes * time.Minute)
+	defer servicesCheckingTicker.Stop()
+
+	configChannel := make(chan *checker.Config)
+	signalsChannel := make(chan os.Signal, 1)
+    signal.Notify(signalsChannel, syscall.SIGINT, syscall.SIGTERM)
+
 	servicesChecker := checker.New(snsNotifier, loggersObject)
-	for {
+	stopProgram := false
+	for !stopProgram {
 		select {
 		case <-servicesCheckingTicker.C:
-			go servicesChecker.Check(&checkerConfig)
+			go servicesChecker.Check(checkerConfig)
+		case <-configCheckingTicker.C:
+			go reloadConfig(configChannel)
+		case newConfig := <-configChannel:
+			checkerConfig = newConfig
+		case <-signalsChannel:
+			stopProgram = true
 		}
 	}
 }
